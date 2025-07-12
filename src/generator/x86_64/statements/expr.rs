635e386a3823fb8244cb80accdfcef5d8fc71615
reversed-R <qiu1287shan@gmail.com>
2025-07-12 14:57:13 +0900

    fix: complete necessary pop for expression statement

--- src/generator/x86_64/statements/expr.rs
+++ src/generator/x86_64/statements/expr.rs
@@ -3,5 +3,7 @@ use crate::{generator::x86_64::Generate, parser::symbols::expressions::Expr};
 impl Generate for Expr {
     fn generate(&self) {
         self.0.generate();
+
+        println!("pop rax");
     }
 }
