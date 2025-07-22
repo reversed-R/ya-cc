f074d8cc9b69c38cc7873da7873d821786d82c47
reversed-R <qiu1287shan@gmail.com>
2025-07-22 17:47:23 +0900

    wip: only one if in a function

--- src/generator/x86_64/statements/if_stmt.rs
+++ src/generator/x86_64/statements/if_stmt.rs
@@ -3,5 +3,29 @@ use crate::{
 };
 
 impl LocalGenerate for IfStmt {
-    fn generate(&self, vars: &mut crate::generator::x86_64::globals::Vars) {}
+    fn generate(&self, vars: &mut crate::generator::x86_64::globals::Vars) {
+        self.cond.generate(vars);
+
+        println!("pop rax");
+        println!("cmp rax, 0");
+
+        match &self.els {
+            Some(els) => {
+                println!("je .Lelse000");
+
+                self.then.generate(vars);
+                println!("jmp .Lend000");
+
+                println!(".Lelse000:");
+                els.generate(vars);
+            }
+            None => {
+                println!("je .Lend000");
+
+                self.then.generate(vars);
+            }
+        }
+
+        println!(".Lend000:");
+    }
 }
