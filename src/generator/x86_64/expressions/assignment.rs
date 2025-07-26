7f92e0673c3df63d2c417d95dadf818cc6062565
reversed-R <qiu1287shan@gmail.com>
2025-07-26 13:53:52 +0900

    feat: code gen assign to dereferenced left hand value

--- src/generator/x86_64/expressions/assignment.rs
+++ src/generator/x86_64/expressions/assignment.rs
@@ -1,60 +1,11 @@
 use crate::{
     generator::x86_64::globals::LocalGenerate,
     parser::symbols::expressions::{
-        assignment::{AssignExpr, AssignExprNode, AssignOperator},
+        assignment::{AssignExpr, AssignOperator},
         primary::Primary,
-        unary::UnaryOperator,
+        unary::{RefUnaryOperator, UnaryOperator},
     },
 };
-
-impl AssignExprNode {
-    fn assignable_variable(&self) -> Option<&String> {
-        // let relat = &self.right.left;
-        // let arithm = &relat.left;
-        // let mul = &arithm.left;
-        // let unary = &mul.left;
-        // let ref_unary = &unary.right;
-        //
-        // if self.right.rights.is_empty()
-        //     && relat.rights.is_empty()
-        //     && arithm.rights.is_empty()
-        //     && mul.rights.is_empty()
-        //     && UnaryOperator::Plus == unary.op
-        //     && ref_unary.ops.is_empty()
-        // {
-        //     if let Primary::Identifier(id) = &ref_unary.right {
-        //         return Some(id);
-        //     }
-        // }
-
-        None
-    }
-}
-
-impl AssignExpr {
-    pub fn assignable_variable(&self) -> Option<&String> {
-        // let equal = &self.left;
-        // let relat = &equal.left;
-        // let arithm = &relat.left;
-        // let mul = &arithm.left;
-        // let unary = &mul.left;
-        // let ref_unary = &unary.right;
-        //
-        // if equal.rights.is_empty()
-        //     && relat.rights.is_empty()
-        //     && arithm.rights.is_empty()
-        //     && mul.rights.is_empty()
-        //     && UnaryOperator::Plus == unary.op
-        //     && ref_unary.ops.is_empty()
-        // {
-        //     if let Primary::Identifier(id) = &ref_unary.right {
-        //         return Some(id);
-        //     }
-        // }
-
-        None
-    }
-}
 
 impl LocalGenerate for AssignExpr {
     fn generate(&self, env: &mut crate::generator::x86_64::globals::Env) {
@@ -63,13 +14,13 @@ impl LocalGenerate for AssignExpr {
         for ass in self.lefts.iter().rev() {
             match ass.left.op {
                 UnaryOperator::Plus => {
-                    println!("pop rdi");
-
                     if ass.left.right.ops.is_empty() {
                         match &ass.left.right.right {
                             Primary::Identifier(id) => {
                                 if let Some(offset) = env.offset(id) {
-                                    ass.op.generate(offset);
+                                    println!("pop rdi");
+                                    ass.op.generate(&format!("[rbp - {offset}]"), "rdi");
+                                    println!("push rdi");
                                 } else {
                                     panic!("Local Variable Not Found");
                                 }
@@ -78,9 +29,32 @@ impl LocalGenerate for AssignExpr {
                                 panic!("Invalid Left Value");
                             }
                         }
-                    }
+                    } else {
+                        for op in &ass.left.right.ops {
+                            match op {
+                                RefUnaryOperator::Ref => {
+                                    panic!("Invalid Left Value");
+                                }
+                                RefUnaryOperator::Deref => match &ass.left.right.right {
+                                    Primary::Identifier(id) => {
+                                        if let Some(offset) = env.offset(id) {
+                                            println!("pop rdi");
 
-                    println!("push rdi");
+                                            println!("mov rax, [rbp - {offset}]");
+                                            println!("mov [rax], rdi");
+
+                                            println!("push rdi");
+                                        } else {
+                                            panic!("Local Variable Not Found");
+                                        }
+                                    }
+                                    _ => {
+                                        panic!("Invalid Left Value");
+                                    }
+                                },
+                            }
+                        }
+                    }
                 }
                 UnaryOperator::Minus => {
                     panic!("Invalid Left Value");
@@ -91,10 +65,10 @@ impl LocalGenerate for AssignExpr {
 }
 
 impl AssignOperator {
-    fn generate(&self, offset: usize) {
+    fn generate(&self, dst: &str, src: &str) {
         match self {
             Self::Assign => {
-                println!("mov [rbp - {offset}], rdi");
+                println!("mov {dst}, {src}");
             }
         }
     }
