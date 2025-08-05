5db1a3ded3029d01f84fc03f41a12a6264d8f4c6
reversed-R <qiu1287shan@gmail.com>
2025-08-05 19:12:17 +0900

    wip: validate primary, wip in function call type validation

--- src/validator/expressions/primary.rs
+++ src/validator/expressions/primary.rs
@@ -1,12 +1,32 @@
 use crate::{
-    parser::symbols::{expressions::primary::Primary, PrimitiveType, Type},
+    parser::symbols::{
+        expressions::primary::{Literal, Primary},
+        PrimitiveType, Type,
+    },
     validator::{Env, ExprTypeValidate, TypeError},
 };
 
 impl ExprTypeValidate for Primary {
     fn validate_type(&self, env: &mut Env) -> Result<Type, TypeError> {
-        // TODO:
-
-        Ok(Type::Primitive(PrimitiveType::Int))
+        match self {
+            Self::Literal(lit) => match lit {
+                Literal::Int(_) => Ok(Type::Primitive(PrimitiveType::Int)),
+                Literal::Float(_) => Ok(Type::Primitive(PrimitiveType::Float)),
+            },
+            Self::Identifier(id) => env
+                .locals
+                .get(id)
+                .ok_or(TypeError::VariableNotFound(id.clone()))
+                .cloned(),
+            Self::Expr(expr) => expr.validate_type(env),
+            Self::FnCall(f) => {
+                if let Some(fsign) = env.fns.get(&f.name) {
+                    // TODO:
+                    Ok(fsign.rtype.clone())
+                } else {
+                    Err(TypeError::VariableNotFound(f.name.clone()))
+                }
+            }
+        }
     }
 }
